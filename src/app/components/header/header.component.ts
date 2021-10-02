19c19d0b5f6085c0ccfa7078da582be040682c26
selcip <eduselcip@gmail.com>
2021-10-02 00:48:34 -0300

    feat: created header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tha-header',
+  template: `
+    <div class="header">
+      <img class="header-logo" src="assets/logo.svg" alt="" />
+    </div>
+  `,
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {}
